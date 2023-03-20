5a279d4fb5b5c3f48e48c05cda55c22ba2a80c08
Estotive <martinyuk.d.i.20@gmail.com>
2023-03-20 22:43:27 +0300

    BD

--- Grand_Basket.sql
+++ Grand_Basket.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: basket_shop
 -- ------------------------------------------------------
--- Server version	8.0.17
+-- Server version	8.0.32
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,12 +23,12 @@ DROP TABLE IF EXISTS `account`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `account` (
-  `iduser` int(11) NOT NULL AUTO_INCREMENT,
+  `iduser` int NOT NULL AUTO_INCREMENT,
   `login` varchar(45) DEFAULT NULL,
   `password` varchar(45) DEFAULT NULL,
   `email` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`iduser`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +37,7 @@ CREATE TABLE `account` (
 
 LOCK TABLES `account` WRITE;
 /*!40000 ALTER TABLE `account` DISABLE KEYS */;
-INSERT INTO `account` VALUES (1,'admin','admin','admin@gmail.com');
+INSERT INTO `account` VALUES (1,'admin','admin','admin@gmail.com'),(2,'Marlen','1234',NULL),(3,'Client','12345',NULL);
 /*!40000 ALTER TABLE `account` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -49,16 +49,10 @@ DROP TABLE IF EXISTS `korzina`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `korzina` (
-  `idkorzina` int(11) NOT NULL AUTO_INCREMENT,
-  `account_iduser` int(11) NOT NULL,
-  `tovar_idtovar` int(11) NOT NULL,
-  `payment` int(11) DEFAULT NULL,
-  PRIMARY KEY (`idkorzina`),
-  KEY `fk_korzina_account_idx` (`account_iduser`),
-  KEY `fk_korzina_tovar1_idx` (`tovar_idtovar`),
-  CONSTRAINT `fk_korzina_account` FOREIGN KEY (`account_iduser`) REFERENCES `account` (`iduser`),
-  CONSTRAINT `fk_korzina_tovar1` FOREIGN KEY (`tovar_idtovar`) REFERENCES `tovar` (`idtovar`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `idkorzina` int NOT NULL AUTO_INCREMENT,
+  `payment` int DEFAULT NULL,
+  PRIMARY KEY (`idkorzina`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -71,6 +65,30 @@ LOCK TABLES `korzina` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `reviews`
+--
+
+DROP TABLE IF EXISTS `reviews`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reviews` (
+  `idviews` int NOT NULL AUTO_INCREMENT,
+  `desc` varchar(300) DEFAULT NULL,
+  `username` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idviews`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reviews`
+--
+
+LOCK TABLES `reviews` WRITE;
+/*!40000 ALTER TABLE `reviews` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reviews` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tovar`
 --
 
@@ -78,16 +96,16 @@ DROP TABLE IF EXISTS `tovar`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tovar` (
-  `idtovar` int(11) NOT NULL AUTO_INCREMENT,
+  `idtovar` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
-  `price` int(11) DEFAULT NULL,
+  `price` int DEFAULT NULL,
   `opisanie` varchar(100) DEFAULT NULL,
   `image` varchar(500) DEFAULT NULL,
-  `type_idtype` int(11) NOT NULL,
+  `type_idtype` int NOT NULL,
   PRIMARY KEY (`idtovar`),
   KEY `fk_tovar_type1_idx` (`type_idtype`),
   CONSTRAINT `fk_tovar_type1` FOREIGN KEY (`type_idtype`) REFERENCES `type` (`idtype`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -96,7 +114,7 @@ CREATE TABLE `tovar` (
 
 LOCK TABLES `tovar` WRITE;
 /*!40000 ALTER TABLE `tovar` DISABLE KEYS */;
-INSERT INTO `tovar` VALUES (1,'Kyrie 7',10000,'Top','',4),(2,'Mavs jersey',5000,'Top',NULL,2),(3,'Headband',2000,'Top',NULL,3),(4,'Jogel J-300',3000,'Top',NULL,1);
+INSERT INTO `tovar` VALUES (1,'Kyrie 7',10000,'Кроссовки','https://www.slamdunk.ru/uploads/monthly_2020_11/kyrie-7-sisterhood-basketball-shoe-5Kj9Lr.jpg.7be1dbf2731b649ebe4f9cbe2ebf03ba.jpg',4),(2,'76ths jersey',5000,'Форма','https://www.buyjersey.cc/upfile/pladd/Adidas-NBA-Philadelphia-76ers-7-Markelle-Fultz-New-Rev-30-Swingman-Blue-Jersey.jpg',2),(3,'Headband',2000,'Аксессуар','https://wallegro.ru/pimg/a/original/116b27/e5f64e9447bd8d043d000df635c8/Opaska-na-glowe-nike-do-biegania-sportowa-frotka',3),(4,'Legacy 8P Jordan',3000,'Мяч','https://static.street-beat.ru/upload/resize_cache/iblock/7e7/1000_1000_0/7e7dcf925a01675a02734463f408a6ac.jpg',1);
 /*!40000 ALTER TABLE `tovar` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -108,10 +126,10 @@ DROP TABLE IF EXISTS `type`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `type` (
-  `idtype` int(11) NOT NULL AUTO_INCREMENT,
+  `idtype` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idtype`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -132,16 +150,15 @@ DROP TABLE IF EXISTS `zakaz`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `zakaz` (
-  `idzakaz` int(11) NOT NULL AUTO_INCREMENT,
-  `korzina_idkorzina` int(11) NOT NULL,
-  `address` varchar(45) DEFAULT NULL,
-  `status` varchar(45) DEFAULT NULL,
-  `phone` int(11) DEFAULT NULL,
-  `sposoboplati` varchar(45) DEFAULT NULL,
+  `idzakaz` int NOT NULL AUTO_INCREMENT,
+  `pokupatel` varchar(100) NOT NULL,
+  `phone` varchar(45) NOT NULL,
+  `address` varchar(115) NOT NULL,
+  `tovar_idtovar` int NOT NULL,
   PRIMARY KEY (`idzakaz`),
-  KEY `fk_zakaz_korzina1_idx` (`korzina_idkorzina`),
-  CONSTRAINT `fk_zakaz_korzina1` FOREIGN KEY (`korzina_idkorzina`) REFERENCES `korzina` (`idkorzina`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  KEY `fk_zakaz_tovar1_idx` (`tovar_idtovar`),
+  CONSTRAINT `fk_zakaz_tovar1` FOREIGN KEY (`tovar_idtovar`) REFERENCES `tovar` (`idtovar`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -150,6 +167,7 @@ CREATE TABLE `zakaz` (
 
 LOCK TABLES `zakaz` WRITE;
 /*!40000 ALTER TABLE `zakaz` DISABLE KEYS */;
+INSERT INTO `zakaz` VALUES (1,'Marlen','+79788295756','Пушкина 3',2),(2,'Marlen','+79788295756','Пушкина 3',2),(3,'Marlen','252452','Пушкина 3',2);
 /*!40000 ALTER TABLE `zakaz` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -162,4 +180,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-01-07 20:06:30
+-- Dump completed on 2023-03-20 21:18:01
